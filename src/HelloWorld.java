534013dfad81bda2677b69c1c10573788c114622
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-01-21 13:44:25 -0600

    Exercise: Java problem 4

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,5 +5,13 @@ public class HelloWorld {
         int myFavoriteNumber = 5;
         System.out.println(myFavoriteNumber);
         //problem2
+        String myString = "This is a string";
+        System.out.println(myString);
+        //problem 3
+//        char myChar = myString;
+        //problem 4
+//        myString = 3.14159;
+        //only works if you use double quotes and doesnt work with numbers
+        System.out.println(myString);
     }
 }
