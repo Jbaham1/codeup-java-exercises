f422bfc0048f8ad61ef0fd279c3a2e4fa8aaac33
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-01-21 13:58:12 -0600

    Java Exercise problem 19

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -11,20 +11,31 @@ public class HelloWorld {
 //        char myChar = myString;
         //problem 4
 //        myString = 3.14159;
-//        only works if you use double quotes and doesnt work with numbers
+//          only works if you use double quotes and doesnt work with numbers
 //        System.out.println(myString);
         //problem 5
         long myNumber;
 //        System.out.println(myNumber);
-//        error
+//          error
         //problem 6
 //        myNumber = 3.14; needs "L" after number
-//problem 7
+//      problem 7
         myNumber = 123L;
         System.out.println(myNumber);
         //problem 8
         myNumber = 123;
         System.out.println(myNumber);
-        //possibly works because it doesnt have a decimal point
+            //possibly works because it doesnt have a decimal point
+        //problem 9
+//        float myNumber = 3.14;
+//        System.out.println(myNumber);
+            //already defined variable
+        //problem 10
+        int x = 5;
+        System.out.println(++x);
+        System.out.println(x);
+            //post decriment prints the original number while pre decriment adds to the original number before it prints.
+        String class = "Class";
+            //error because class is protected
     }
 }
