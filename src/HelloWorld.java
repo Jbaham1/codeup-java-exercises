36c7562a67e81ddda5b725fa972810c6fa1c70f6
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-01-21 13:39:10 -0600

    java exercise

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        System.out.println("Hello, World!");
+        //problem 1
+        int myFavoriteNumber = 5;
+        System.out.println(myFavoriteNumber);
+        //problem2
     }
 }
