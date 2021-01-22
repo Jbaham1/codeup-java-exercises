13b7caf46c1381c6128a4fd7ec8961f7fe4d9240
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-01-22 14:43:43 -0600

    updates

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -8,23 +8,23 @@ public class ConsoleExercises {
         System.out.format("The value of pi is approximately %.2f%n", pi);
 ////Exercise 2 problem 1
         Scanner input = new Scanner(System.in);
-//        System.out.print("Enter an integer: ");
-//        int userInterger = input.nextInt();
+        System.out.print("Enter an integer: ");
+        int userInterger = input.nextInt();
 //            //can only enter interger or you'll get an exception in thread main error
 //        //you can enter more than three words, but the system will only print 3 unless prompted otherwise
 //        // problem 2
-//        System.out.println("Enter 3 words: ");
-//        String userInput1 = input.next();
-//        String userInput2 = input.next();
-//        String userInput3 = input.next();
-//        System.out.println("You entered: -->\"" + userInterger + "\"<--");
-//        System.out.println("You entered: -->\"" + userInput1 + "\"<--");
-//        System.out.println("You entered: -->\"" + userInput2 + "\"<--");
-//        System.out.println("You entered: -->\"" + userInput3 + "\"<--");
+        System.out.println("Enter 3 words: ");
+        String userInput1 = input.next();
+        String userInput2 = input.next();
+        String userInput3 = input.next();
+        System.out.println("You entered: -->\"" + userInterger + "\"<--");
+        System.out.println("You entered: -->\"" + userInput1 + "\"<--");
+        System.out.println("You entered: -->\"" + userInput2 + "\"<--");
+        System.out.println("You entered: -->\"" + userInput3 + "\"<--");
 //Problem 3
-//        System.out.println("Enter a sentence: ");
-//        String userInput4 = input.next();
-//        System.out.println("Your sentence is : -->\"" + userInput4 + "\"<--");
+        System.out.println("Enter a sentence: ");
+        String userInput4 = input.next();
+        System.out.println("Your sentence is : -->\"" + userInput4 + "\"<--");
         //only captured the first word
         // problem 4
         System.out.println("Enter another sentence: ");
