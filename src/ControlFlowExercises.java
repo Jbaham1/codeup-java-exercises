fa1c0f488d86f206365c2d8726792e0c9b8018d4
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-01-22 16:49:02 -0600

    Exercise: ControlFlowExercises

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,18 @@
+public class ControlFlowExercises {
+    public static void main(String[] args){
+        //Problem 1a
+        int i = 5;
+        while(i <= 15){
+            System.out.println("i is " + i);
+            i++;
+        }
+        //Problem 1b
+       int num = 0;
+        do{
+            System.out.println("i is now " + num);
+            num++;
+            num++;
+        } while(num <=100);
+//Problem 1C
+    }
+}
