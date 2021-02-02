3b6fe5de207d59ceced7394f5eeb05829877e74e
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-02 15:58:45 -0600

    ShapeTest

--- src/shapes/ShapeTest.java
+++ src/shapes/ShapeTest.java
@@ -3,6 +3,8 @@ package shapes;
 public class ShapeTest {
     public static void main(String args[]) {
         Rectangle box1 = new Rectangle(4, 5);
+        System.out.println(Rectangle.getArea());
+        System.out.println(Rectangle.getPerimeter());
     }
 }
 
