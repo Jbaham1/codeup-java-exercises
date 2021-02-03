a4121c25511bc635409922390469f4a15c971de9
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-03 10:53:15 -0600

    override correction

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,9 +1,20 @@
 package shapes;
 
 public class Square extends Rectangle {
-
+    double side = 5;
     public Square(double side) {
         super(side, side);
+
     }
+//    @Override
+    public double getArea(double side){
+        double area = Math.pow(side, 2);
+        return  area;
+    }
+    public double getPerimeter(double side){
+        double perimeter = 4 * side;
+        return perimeter;
+    }
+
 
 }
