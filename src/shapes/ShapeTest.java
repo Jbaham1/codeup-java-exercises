986455a9691533a96c20a5f836a9ec65960bfafc
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-04 16:06:07 -0600

    shape test

--- src/shapes/ShapeTest.java
+++ src/shapes/ShapeTest.java
@@ -2,6 +2,7 @@ package shapes;
 
 public class ShapeTest {
     public static void main(String args[]) {
+//        Measurable myShape =
         Rectangle box1 = new Rectangle(5, 4);
         System.out.println("The Area is "+box1.getArea());
         System.out.println("The Perimeter is "+box1.getPerimeter());
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,10 +1,9 @@
 package shapes;
 
-public class Square extends Quadrilateral implements Measurable {
-    public Square(double side) {
+public class Square extends Rectangle {
+    protected Square(double side) {
         super(side, side);
     }
-
     @Override
     public void setLength(double side) {
 
@@ -26,18 +25,3 @@ public class Square extends Quadrilateral implements Measurable {
     }
 }
 
-
-//////    double side = 5;
-////    public Square(double side) {
-////        super(side, side);
-////
-////    }
-////    @Override
-////    public double getArea(){
-////       return Math.pow(this.length, 2);
-////
-////    }
-////    public double getPerimeter(){
-////     return  4 * this.length;
-////
-////    }
