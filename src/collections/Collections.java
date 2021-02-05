c65a42565f7560b2a3a248565dfd2258f048600d
Jeremiah Baham <baham.jeremiah20@gmail.com>
2021-02-05 10:29:53 -0600

    collections lecture

--- src/collections/Collections.java
+++ src/collections/Collections.java
@@ -20,6 +20,7 @@ public class Collections {
         roasts.add("dark");
 
         System.out.println(roasts);
+
         // 2. Check to see if the list contains "dark" , and then "espresso"
         boolean result;
         result = roasts.contains("dark");
@@ -27,9 +28,11 @@ public class Collections {
 
         result = roasts.contains("espresso");
         System.out.println(result ? "Contains espresso!" : "Does not contain espresso!");
+
         // 3. Find the last index of "medium" in the array
         int index = roasts.lastIndexOf("medium");
         System.out.println("Last index of medium is: " + index);
+
         // 4. Check if the array list is empty
         System.out.println(roasts.isEmpty() ? "List is empty" : "List is not empty");
 
@@ -62,16 +65,37 @@ public class Collections {
         // 1. Create a HashMap called usernames that contains:
         //   a. first name
         //   b. Github username
+        HashMap<String, String> usernames = new HashMap<>();
+        usernames.put("Casey", "fridaynext");
+        usernames.put("Daniel", "danielfryar");
+        usernames.put("Fernando", "fmendoza");
+        System.out.println(usernames);
         // 2. re-initialize the HashMap using the .clear() method
+        usernames.clear();
         // 3. use the .put() method to add "Casey" -> "fridaynext" back to the map
+        usernames.put("Casey", "fridaynext");
         // 4. use the .putIfAbsent() method to add
         //   a. "Vivian" -> "viviancan"
         //   b. "Casey" -> "XpenguinX"
+
+        usernames.putIfAbsent("Daniel", "danielfryar");
+        usernames.putIfAbsent("Casey", "XpenguinX");
+        System.out.println(usernames);
         // 5. What happend with the .putIfAbsent()? Did both items get added? Why/why not?
+             //Key "Casey" was present so we didn not add that key/value pair with XpenguinX
+
         // 6. Use the .remove() method to remove "Casey"
-        // 7. Use the .replace() method to change Vivian's username to "viviancandev"
+        usernames.remove("Casey");
+        System.out.println(usernames);
+        // 7. Use the .replace() method to change Daniel's username to "danielfryardev"
+        usernames.replace("Daniel", "danielfryardev");
+        System.out.println(usernames);
+
         // 8. Use the .clear() method to clear the map
+        usernames.clear();
+
         // 9. Use the .isEmpty() method to verify that it was cleared
+        System.out.println(usernames.isEmpty() ? "HasMap Empty!" : "HasMap is not empty!");
     }
 }
 
